4bdd5e8663b7a0b50c63abb091d58776e157ce59
Shion <2637594835@qq.com>
2025-06-22 16:42:35 +0800

    Delete the build files.

--- build/.qt/QtDeployTargets-RelWithDebInfo.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Chatroom_FILE D:/Program/C++code/Qt/Chatroom/build/RelWithDebInfo/Chatroom.exe)
-set(__QT_DEPLOY_TARGET_Chatroom_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Chatroom_RUNTIME_DLLS D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Network.dll;D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Gui.dll;D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_8_2_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Chatroom_FILE D:/Program/C++code/Qt/Chatroom/build/Desktop_Qt_6_8_2_MSVC2022_64bit-Debug/Chatroom.exe)
-set(__QT_DEPLOY_TARGET_Chatroom_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Chatroom_RUNTIME_DLLS D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Networkd.dll;D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Guid.dll;D:/Program/Qt/6.8.2/msvc2022_64/bin/Qt6Cored.dll)
